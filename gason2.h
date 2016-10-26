c211cb71782a7f77c8ce4bb59f52a0b55134e8a9
Ivan Vashchaev <vivkin@gmail.com>
2016-10-26 03:59:25 +0300

    parse_string is not static now and use _stack explicit

--- gason2.h
+++ gason2.h
@@ -330,60 +330,6 @@ struct parser {
         }
     }
 
-    static value parse_string(vector<value> &v, stream &s) {
-        size_t offset = v.size();
-        size_t size = 0;
-        for (;;) {
-            v.resize(v.size() + 8);
-            char *span = v[offset].s;
-
-            for (size_t size_end = size + sizeof(value) * 7; size < size_end; ++size) {
-                span[size] = s.getch();
-                switch (span[size]) {
-                case '"':
-                    span[size] = '\0';
-                    v.resize(offset + (size + 8) / 8);
-                    return {offset, string_tag};
-
-                case '\\': {
-                    int cp = unescape(s);
-                    if (cp >= 0xD800 && cp <= 0xDBFF) {
-                        if (s.getch() != '\\')
-                            return error_invalid_surrogate_pair;
-                        int low = unescape(s);
-                        if (low < 0xDC00 && low > 0xDFFF)
-                            return error_invalid_surrogate_pair;
-                        cp = 0x10000 + ((cp & 0x3FF) << 10) + (low & 0x3FF);
-                    }
-
-                    if (cp < 0)
-                        return error_invalid_string_escape;
-                    else if (cp < 0x80) {
-                        span[size] = (char)cp;
-                    } else if (cp < 0x800) {
-                        span[size] = 0xC0 | ((char)(cp >> 6));
-                        span[++size] = 0x80 | (cp & 0x3F);
-                    } else if (cp < 0xFFFF) {
-                        span[size] = 0xE0 | ((char)(cp >> 12));
-                        span[++size] = 0x80 | ((cp >> 6) & 0x3F);
-                        span[++size] = 0x80 | (cp & 0x3F);
-                    } else {
-                        span[size] = 0xF0 | ((char)(cp >> 18));
-                        span[++size] = 0x80 | ((cp >> 12) & 0x3F);
-                        span[++size] = 0x80 | ((cp >> 6) & 0x3F);
-                        span[++size] = 0x80 | (cp & 0x3F);
-                    }
-                    continue;
-                }
-
-                default:
-                    if ((unsigned int)span[size] < ' ')
-                        return error_invalid_string_char;
-                }
-            }
-        }
-    }
-
     vector<value> _backlog;
     vector<value> _stack;
 
@@ -391,7 +337,7 @@ struct parser {
         switch (s.skipws()) {
         case '"':
             s.getch();
-            return parse_string(_stack, s);
+            return parse_string(s);
         case ',':
             s.getch();
             return token_value_separator;
@@ -436,6 +382,55 @@ struct parser {
             break;
         }
         return error_unexpected_character;
+    }
+
+    value parse_string(stream &s) {
+        size_t offset = _stack.size();
+        size_t length = 0;
+        for (;;) {
+            _stack.resize(_stack.size() + 8);
+            char *span = _stack[offset].s;
+            for (size_t size_end = length + sizeof(value) * 7; length < size_end; ++length) {
+                switch (span[length] = s.getch()) {
+                case '"':
+                    span[length] = '\0';
+                    _stack.resize(offset + (length + 8) / 8);
+                    return {offset, string_tag};
+                case '\\': {
+                    int cp = unescape(s);
+                    if (cp >= 0xD800 && cp <= 0xDBFF) {
+                        if (s.getch() != '\\')
+                            return error_invalid_surrogate_pair;
+                        int low = unescape(s);
+                        if (low < 0xDC00 && low > 0xDFFF)
+                            return error_invalid_surrogate_pair;
+                        cp = 0x10000 + ((cp & 0x3FF) << 10) + (low & 0x3FF);
+                    }
+                    if (cp < 0)
+                        return error_invalid_string_escape;
+                    else if (cp < 0x80) {
+                        span[length] = (char)cp;
+                    } else if (cp < 0x800) {
+                        span[length] = 0xC0 | ((char)(cp >> 6));
+                        span[++length] = 0x80 | (cp & 0x3F);
+                    } else if (cp < 0xFFFF) {
+                        span[length] = 0xE0 | ((char)(cp >> 12));
+                        span[++length] = 0x80 | ((cp >> 6) & 0x3F);
+                        span[++length] = 0x80 | (cp & 0x3F);
+                    } else {
+                        span[length] = 0xF0 | ((char)(cp >> 18));
+                        span[++length] = 0x80 | ((cp >> 12) & 0x3F);
+                        span[++length] = 0x80 | ((cp >> 6) & 0x3F);
+                        span[++length] = 0x80 | (cp & 0x3F);
+                    }
+                    continue;
+                }
+                default:
+                    if ((unsigned int)span[length] < ' ')
+                        return error_invalid_string_char;
+                }
+            }
+        }
     }
 
     value parse_array(stream &s) {
