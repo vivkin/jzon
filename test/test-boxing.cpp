a5be1abc2e58077f950b6231cbdf84ba9464f628
Ivan Vashchaev <vivkin@gmail.com>
2016-11-24 13:33:31 +0300

    test boxing

---
+++ test/test-boxing.cpp
@@ -0,0 +1,68 @@
+#include "doctest.h"
+#include "gason2.h"
+#include <float.h>
+#include <math.h>
+
+using namespace gason2;
+
+#define TEST_NOTNAN(x)              \
+    CHECK_FALSE(value{x}.is_nan()); \
+    CHECK_FALSE(value{-x}.is_nan())
+
+TEST_CASE("[gason] boxing double") {
+    TEST_NOTNAN(0.0);
+    TEST_NOTNAN(1.0);
+    TEST_NOTNAN(1.0 / 3.0);
+    TEST_NOTNAN(5.45);
+    TEST_NOTNAN(7.62);
+    TEST_NOTNAN(1e40);
+    TEST_NOTNAN(M_E);
+    TEST_NOTNAN(M_LOG2E);
+    TEST_NOTNAN(M_LOG10E);
+    TEST_NOTNAN(M_LN2);
+    TEST_NOTNAN(M_LN10);
+    TEST_NOTNAN(M_PI);
+    TEST_NOTNAN(M_PI_2);
+    TEST_NOTNAN(M_PI_4);
+    TEST_NOTNAN(M_1_PI);
+    TEST_NOTNAN(M_2_PI);
+    TEST_NOTNAN(M_2_SQRTPI);
+    TEST_NOTNAN(M_SQRT2);
+    TEST_NOTNAN(M_SQRT1_2);
+    TEST_NOTNAN(DBL_MIN);
+    TEST_NOTNAN(DBL_EPSILON);
+    TEST_NOTNAN(DBL_MAX);
+    TEST_NOTNAN(INFINITY);
+    TEST_NOTNAN(NAN);
+}
+
+#define TEST_TYPE(x)                  \
+    CHECK(value{x}.is_nan());         \
+    CHECK_FALSE(value{x}.is_error()); \
+    CHECK(value{x}.tag.type == x)
+
+TEST_CASE("[gason] boxing types") {
+    TEST_TYPE(type::null);
+    TEST_TYPE(type::boolean);
+    TEST_TYPE(type::string);
+    TEST_TYPE(type::array);
+    TEST_TYPE(type::object);
+}
+
+#define TEST_ERROR(x)           \
+    CHECK(value{x}.is_nan());   \
+    CHECK(value{x}.is_error()); \
+    CHECK(value{x}.tag.error == x)
+
+TEST_CASE("[gason] boxing errors") {
+    TEST_ERROR(error::expecting_string);
+    TEST_ERROR(error::expecting_value);
+    TEST_ERROR(error::invalid_literal_name);
+    TEST_ERROR(error::invalid_number);
+    TEST_ERROR(error::invalid_string_char);
+    TEST_ERROR(error::invalid_string_escape);
+    TEST_ERROR(error::invalid_surrogate_pair);
+    TEST_ERROR(error::missing_colon);
+    TEST_ERROR(error::missing_comma_or_bracket);
+    TEST_ERROR(error::unexpected_character);
+}
--- test/tests.cpp
+++ test/tests.cpp
@@ -1,37 +1,8 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 #include "gason2.h"
-#include "gason2dump.h"
-#include <float.h>
-#include <math.h>
-#include <stdio.h>
 
-void pbox(gason2::value v) {
-    printf("%2d %6d %8x %10u %13g\n", v.number == v.number, v.is_nan(), static_cast<unsigned int>(v.tag.type), v.payload, v.number);
-}
-
-TEST_CASE("boxing") {
-    printf("%2s %6s %4s %15s %13s\n", "==", "is_nan", "tag", "payload", "number");
-
-    double numbers[] = {0.0, 1.0, 1.0 / 3.0, 5.45, 7.62, 1e40, DBL_MIN, DBL_EPSILON, DBL_MAX, INFINITY, NAN};
-
-    for (auto n : numbers) {
-        pbox(-n);
-        pbox(n);
-    }
-
-    pbox(gason2::error::expecting_value);
-    pbox(gason2::error::unexpected_character);
-    pbox({gason2::type::null, 0});
-    pbox({gason2::type::boolean, false});
-    pbox({gason2::type::boolean, true});
-    pbox({gason2::type::string, 0xDEADBEEF});
-    pbox({gason2::type::string, 0xBADCAB1E});
-    pbox({gason2::type::array, ~1ul});
-    pbox({gason2::type::object, 0xFFFFFFFFul});
-}
-
-TEST_CASE("obvious") {
+TEST_CASE("[gason] parsing") {
     gason2::document doc;
 
     CHECK_FALSE(doc.parse(""));
@@ -53,7 +24,7 @@ TEST_CASE("obvious") {
     CHECK(doc.is_object());
     CHECK(doc.size() == 4);
     CHECK_EQ(doc["alpha"].to_string(), "abcdefghijklmnopqrstuvwyz");
-    CHECK(doc["num"].to_string() == "abcdefghijklmnopqrstuvwyz");
+    CHECK(doc["num"].to_string("haha") == "haha");
     CHECK(doc["num"].to_number() == 123456789);
     CHECK(doc[100][500].to_number(100500) == 100500);
     CHECK(doc["empty"].is_object());
